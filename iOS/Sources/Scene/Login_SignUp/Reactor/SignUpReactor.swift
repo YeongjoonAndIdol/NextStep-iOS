7c88347f3bae60e1181356ee5f8248a6fd104f51
Daehee Kim <0824dh@naver.com>
2022-09-13 22:32:01 +0900

    feat :: SignUpReactor file

---
+++ iOS/Sources/Scene/Login_SignUp/Reactor/SignUpReactor.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpReactor.swift
+//  Next-Stap
+//
+//  Created by 김대희 on 2022/09/13.
+//  Copyright © 2022 com.DMS. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+
+class SignUpReactor: Reactor {
+
+    private let disposeBag: DisposeBag = .init()
+    let initialState: State
+
+    enum Action {
+    }
+    enum Mutation {
+    }
+    struct State {
+    }
+
+    init() {
+        initialState = State()
+
+    }
+
+}
