0e939f265d3351af331b5d121b6eb09dcf6a434e
Daehee Kim <0824dh@naver.com>
2022-09-19 00:38:38 +0900

    feat :: signupVC 변수 선언

--- iOS/Sources/Scene/Login_SignUp/VC/SignUpVC.swift
+++ iOS/Sources/Scene/Login_SignUp/VC/SignUpVC.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2022 com.DMS. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SignUpVC: BaseVC<SignUpReactor> {
+
+    private let nameTextFieldBackView = UIView()
+    private let idTextFieldBackView = UIView()
+    private let passwordTextFieldBackView = UIView()
+
+    private let nameTextField = UITextField()
+    private let idTextField = UITextField()
+    private let passwordTextField = UITextField()
+
+    private let nameBottomLabel = UILabel()
+    private let idBottomLabel = UILabel()
+    private let passwordBottomLabel = UILabel()
+
+    private let doneButton = UIButton().then {
+        $0.setTitleColor(NextStapColor.surfaceColor.color, for: .normal)
+        $0.setTitle("완료", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16, weight: .semibold)
+        $0.backgroundColor = NextStapColor.subColor3.color
+        $0.layer.cornerRadius = 10
+    }
+
+    override func addView() {
+        [
+            doneButton
+        ].forEach {
+            view.addSubview($0)
+        }
+    }
+
+    override func setLayout() {
+        doneButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-26)
+            $0.leading.trailing.equalTo(view).inset(16)
+            $0.height.equalTo(54)
+        }
+        doneButton.makeDoneButtonShadows()
+    }
+
+}
