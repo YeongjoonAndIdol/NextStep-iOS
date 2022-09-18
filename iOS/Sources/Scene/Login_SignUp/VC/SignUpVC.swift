e85252279f00afddeb19fb07b6c1eedb1457ef45
Daehee Kim <0824dh@naver.com>
2022-09-19 01:27:48 +0900

    feat :: configureVC setLayout

--- iOS/Sources/Scene/Login_SignUp/VC/SignUpVC.swift
+++ iOS/Sources/Scene/Login_SignUp/VC/SignUpVC.swift
@@ -26,25 +26,111 @@ class SignUpVC: BaseVC<SignUpReactor> {
         $0.setTitleColor(NextStapColor.surfaceColor.color, for: .normal)
         $0.setTitle("완료", for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 16, weight: .semibold)
-        $0.backgroundColor = NextStapColor.subColor3.color
+        $0.backgroundColor = NextStapColor.buttonDisabledColor.color
         $0.layer.cornerRadius = 10
     }
 
     override func addView() {
+
         [
-            doneButton
-        ].forEach {
+            doneButton,
+            nameTextFieldBackView,
+            idTextFieldBackView,
+            passwordTextFieldBackView,
+            nameBottomLabel,
+            idBottomLabel,
+            passwordBottomLabel
+        ]
+            .forEach {
             view.addSubview($0)
         }
+
+        nameTextFieldBackView.addSubview(nameTextField)
+        idTextFieldBackView.addSubview(idTextField)
+        passwordTextFieldBackView.addSubview(passwordTextField)
+    }
+
+    override func configureVC() {
+
+        [nameTextFieldBackView, idTextFieldBackView, passwordTextFieldBackView].forEach {
+            $0.layer.cornerRadius = 10
+            $0.layer.borderWidth = 1
+            $0.layer.borderColor = NextStapColor.gary2.color.cgColor
+            $0.backgroundColor = NextStapColor.surfaceColor.color
+        }
+
+        [nameBottomLabel, idBottomLabel, passwordBottomLabel].forEach {
+            $0.textColor = NextStapColor.errorColor.color
+            $0.font = .systemFont(ofSize: 12, weight: .regular)
+        }
+
+        [nameTextField, idTextField, passwordTextField].forEach {
+            $0.textColor = NextStapColor.onSurfaceColor.color
+            $0.tintColor = NextStapColor.mainColor.color
+        }
+
+        nameTextField.attributedPlaceholder = NSAttributedString(
+            string: "이름을 입력해주세요.",
+            attributes: [NSAttributedString.Key.foregroundColor: NextStapColor.textButtonDisabledColor.color]
+        )
+
+        idTextField.attributedPlaceholder = NSAttributedString(
+            string: "아이디를 입력해주세요.",
+            attributes: [NSAttributedString.Key.foregroundColor: NextStapColor.textButtonDisabledColor.color]
+        )
+
+        passwordTextField.attributedPlaceholder = NSAttributedString(
+            string: "비밀번호를 입력해주세요.",
+            attributes: [NSAttributedString.Key.foregroundColor: NextStapColor.textButtonDisabledColor.color]
+        )
+
     }
 
     override func setLayout() {
+
+        nameTextFieldBackView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view).inset(16)
+            $0.height.equalTo(48)
+            $0.bottom.equalTo(idTextFieldBackView.snp.top).offset(-40)
+        }
+
+        idTextFieldBackView.snp.makeConstraints {
+            $0.centerY.equalTo(view).offset(-80)
+            $0.leading.trailing.equalTo(view).inset(16)
+            $0.height.equalTo(48)
+        }
+
+        passwordTextFieldBackView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view).inset(16)
+            $0.height.equalTo(48)
+            $0.top.equalTo(idTextFieldBackView.snp.bottom).offset(40)
+        }
+
+        [nameTextField, idTextField, passwordTextField].forEach { textField in
+            textField.snp.makeConstraints {
+                $0.leading.trailing.equalToSuperview().inset(16)
+                $0.top.bottom.equalToSuperview().inset(11)
+            }
+        }
+
+        let bottomLabel: [UILabel] = [nameBottomLabel, idBottomLabel, passwordBottomLabel]
+        let textFieldBackView: [UIView] = [nameTextFieldBackView, idTextFieldBackView, passwordTextFieldBackView]
+
+        for count in 0..<3 {
+            bottomLabel[count].snp.makeConstraints {
+                $0.leading.trailing.equalTo(0).inset(19)
+                $0.height.equalTo(12)
+                $0.top.equalTo(textFieldBackView[count].snp.bottom).offset(8)
+            }
+        }
+
         doneButton.snp.makeConstraints {
             $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-26)
             $0.leading.trailing.equalTo(view).inset(16)
             $0.height.equalTo(54)
         }
         doneButton.makeDoneButtonShadows()
+
     }
 
 }
