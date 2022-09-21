a444c9ec1dfeabf52d634fd89d4a39224fa63e59
Daehee Kim <0824dh@naver.com>
2022-09-21 13:14:42 +0900

    feat :: HomeReactor.swift

---
+++ iOS/Sources/Scene/HomeReactor.swift
@@ -0,0 +1,28 @@
+//
+//  HomeReactor.swift
+//  Next-Stap
+//
+//  Created by 김대희 on 2022/09/21.
+//  Copyright © 2022 com.DMS. All rights reserved.
+//
+
+import ReactorKit
+
+class HomeReactor: Reactor {
+
+    private let disposeBag: DisposeBag = .init()
+    let initialState: State
+
+    enum Action {
+    }
+    enum Mutation {
+    }
+    struct State {
+    }
+
+    init() {
+        initialState = State()
+
+    }
+
+}
