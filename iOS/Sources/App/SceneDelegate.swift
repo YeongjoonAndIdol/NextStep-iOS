7ed1992d76c309a301d548afea390302e2f03099
Daehee Kim <0824dh@naver.com>
2022-09-25 22:40:56 +0900

    refector :: RootVC TabBarVC 로 변경

--- iOS/Sources/App/SceneDelegate.swift
+++ iOS/Sources/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         let mainViewController = TabBarVC()
-        let naviMainViewController = BaseNC(rootViewController: mainViewController)
+        let naviMainViewController = mainViewController
         window?.rootViewController = naviMainViewController
         window?.makeKeyAndVisible()
 
