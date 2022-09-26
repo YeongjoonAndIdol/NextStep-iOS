719fc020afb6d589f576e4cf2a23d7e4f0719e0c
Daehee Kim <0824dh@naver.com>
2022-09-26 22:20:32 +0900

    feat :: BaseTC

--- iOS/Sources/Base/BaseTC.swift
+++ iOS/Sources/Base/BaseTC.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2022 com.DMS. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class BaseTC: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addView()
+        self.setLayout()
+        self.configureVC()
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func addView() {}
+    func setLayout() {}
+    func configureVC() {}
+
+}
