35b562d45175ff675ec909808b663f81166b2005
Daehee Kim <0824dh@naver.com>
2022-09-27 15:19:33 +0900

    feat :: QuestListCell

--- iOS/Sources/Scene/Home/Cell/QuestListCell.swift
+++ iOS/Sources/Scene/Home/Cell/QuestListCell.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2022 com.DMS. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class QuestListCell: BaseTC {
+
+    let titleLabel = UILabel().then {
+        $0.textColor = NextStapColor.onSurfaceColor.color
+        $0.font = .systemFont(ofSize: 16, weight: .semibold)
+    }
+
+    let checkButton = CheckBox()
+
+    override func addView() {
+        [titleLabel, checkButton].forEach {
+            contentView.addSubview($0)
+        }
+    }
+
+    override func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.bottom.equalTo(contentView).inset(15)
+            $0.left.equalTo(40)
+            $0.right.equalTo(-84)
+        }
+
+        checkButton.snp.makeConstraints {
+            $0.right.equalTo(40)
+            $0.height.width.equalTo(24)
+            $0.centerY.equalTo(contentView)
+        }
+    }
+}
