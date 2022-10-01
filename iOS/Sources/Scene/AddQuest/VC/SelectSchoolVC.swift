96023f7595593dfe682832df5a5f51a129f4852a
Daehee Kim <0824dh@naver.com>
2022-10-01 16:52:40 +0900

    feat :: SelectSchoolVC

---
+++ iOS/Sources/Scene/AddQuest/VC/SelectSchoolVC.swift
@@ -0,0 +1,13 @@
+//
+//  SelectSchoolVC.swift
+//  Next-Stap
+//
+//  Created by 김대희 on 2022/10/01.
+//  Copyright © 2022 com.DMS. All rights reserved.
+//
+
+import UIKit
+
+class SelectSchoolVC: UIViewController {
+    private let stackView = UIStackView()
+}
