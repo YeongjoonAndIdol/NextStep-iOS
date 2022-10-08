fb64215e4b83962030d69ae2d40e46585b2a1f5c
Daehee Kim <0824dh@naver.com>
2022-10-08 21:20:58 +0900

    feat :: AddQuestReactor

--- iOS/Sources/Scene/AddQuest/Reactor/AddQuestReactor.swift
+++ iOS/Sources/Scene/AddQuest/Reactor/AddQuestReactor.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2022 com.DMS. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+
+class AddQuestReactor: Reactor {
+
+    private let disposeBag: DisposeBag = .init()
+    let initialState: State
+
+    enum Action { 
+    }
+    enum Mutation {
+    }
+    struct State {
+    }
+
+    init() {
+        initialState = State()
+
+    }
+
+}
