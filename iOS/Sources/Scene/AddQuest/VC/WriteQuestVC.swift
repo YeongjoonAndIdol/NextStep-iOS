b0fc5bc0ad68394e59aef9c741a904159a72aeb2
Daehee Kim <0824dh@naver.com>
2022-10-12 09:22:21 +0900

    feat :: 변수 생성

--- iOS/Sources/Scene/AddQuest/VC/WriteQuestVC.swift
+++ iOS/Sources/Scene/AddQuest/VC/WriteQuestVC.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+    typealias NextStapColor = NextStapAsset.Color
+    typealias NextStapImage = NextStapAsset.Assets
+    private var disposeBag: DisposeBag = .init()
+    private var categoryNumber = 0
+
+    private let titleTextField = UITextField().then {
+        $0.textColor = NextStapColor.onSurfaceColor.color
+        $0.tintColor = NextStapColor.mainColor.color
+        $0.font = .systemFont(ofSize: 20, weight: .regular)
+    }
+
+    private let contentTextView = UITextView().then {
+        $0.font = .systemFont(ofSize: 16, weight: .regular)
+        $0.tintColor = NextStapColor.onSurfaceColor.color
+        $0.textColor = NextStapColor.textButtonDisabledColor.color
+        $0.textContainer.maximumNumberOfLines = 5
+    }
