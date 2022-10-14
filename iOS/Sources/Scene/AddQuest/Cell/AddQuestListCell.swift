28362bda7a6cfb27abc0eb32d65e410dad29aaec
Daehee Kim <0824dh@naver.com>
2022-10-14 10:19:07 +0900

    feat :: dummydata

--- iOS/Sources/Scene/AddQuest/Cell/AddQuestListCell.swift
+++ iOS/Sources/Scene/AddQuest/Cell/AddQuestListCell.swift
@@ -48,42 +48,50 @@ class AddQuestListCell: BaseTC {
         ].forEach {
             contentView.addSubview($0)
         }
+
+        contentView.backgroundColor = NextStapColor.surfaceColor.color
+        leftBar.backgroundColor = NextStapColor.subColor3.color
+        categoryImage.image = NextStapImage.small3.image
+        categorytextLabel.text = "자율활동"
+        questTitleLabel.text = "영준이 죽이기"
+        contentTextLabel.text = "연예하는 영준이를 죽이는 활동을 해보아요"
+
     }
 
     override func setLayout() {
         leftBar.snp.makeConstraints {
             $0.leading.equalTo(30)
-            $0.top.bottom.equalTo(0)
+            $0.top.bottom.equalTo(contentView).inset(5)
             $0.width.equalTo(4)
         }
         categoryImage.snp.makeConstraints {
-            $0.leading.equalTo(16)
+            $0.leading.equalTo(46)
             $0.width.height.equalTo(24)
             $0.top.equalTo(1)
         }
         categorytextLabel.snp.makeConstraints {
-            $0.leading.equalTo(52)
+            $0.leading.equalTo(82)
             $0.top.equalTo(4)
             $0.height.equalTo(19)
             $0.trailing.equalTo(-16)
         }
         questTitleLabel.snp.makeConstraints {
             $0.top.equalTo(categorytextLabel.snp.bottom).offset(11)
-            $0.leading.equalTo(14)
-            $0.trailing.equalTo(-16)
+            $0.leading.equalTo(44)
+            $0.trailing.equalTo(-44)
             $0.height.equalTo(22)
         }
         contentTextLabel.snp.makeConstraints {
             $0.top.equalTo(questTitleLabel.snp.bottom).offset(4)
-            $0.leading.equalTo(14)
-            $0.trailing.equalTo(-16)
+            $0.leading.equalTo(44)
+            $0.trailing.equalTo(-44)
         }
         pointLabel.snp.makeConstraints {
             $0.top.equalTo(contentTextLabel.snp.bottom).offset(4)
-            $0.leading.equalTo(14)
-            $0.trailing.equalTo(-16)
+            $0.leading.equalTo(44)
+            $0.trailing.equalTo(-44)
             $0.height.equalTo(16)
-            $0.bottom.equalTo(2)
+            $0.bottom.equalTo(-2)
         }
     }
 }
