3a3eeb4d31a3b6eba8be736b4a0d46b125b5e524
Daehee Kim <0824dh@naver.com>
2022-10-17 10:02:53 +0900

    feat :: DetailQuestVC backView shadow 설정

--- iOS/Sources/Extension/UIView+.swift
+++ iOS/Sources/Extension/UIView+.swift
@@ -16,4 +16,11 @@ extension UIView {
         self.layer.shadowRadius = 8
         self.layer.shadowOffset = CGSize(width: 0, height: 4)
     }
+    func makeBackViewShadows() {
+        self.layer.shadowPath = nil
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+        self.layer.shadowOpacity = 1
+        self.layer.shadowRadius = 16
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+    }
 }
