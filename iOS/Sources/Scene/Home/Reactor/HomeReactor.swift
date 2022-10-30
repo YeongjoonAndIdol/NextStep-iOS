169fbaf1e5e4236029fce3d131fdb27bcefcdee3
Daehee Kim <0824dh@naver.com>
2022-10-30 20:55:17 +0900

    feat :: HomeReactor

--- iOS/Sources/Scene/Home/Reactor/HomeReactor.swift
+++ iOS/Sources/Scene/Home/Reactor/HomeReactor.swift
@@ -1,28 +1,57 @@
-//
-//  HomeReactor.swift
-//  Next-Stap
-//
-//  Created by 김대희 on 2022/09/21.
-//  Copyright © 2022 com.DMS. All rights reserved.
-//
-
+import Foundation
 import ReactorKit
+import Moya
+import RxMoya
 
 class HomeReactor: Reactor {
-
     private let disposeBag: DisposeBag = .init()
+    private let provider = MoyaProvider<NextStapAPI>()
     let initialState: State
 
     enum Action {
+        case viewDidLoad
     }
     enum Mutation {
+        case fetchQuestList
     }
     struct State {
+        var progress: Int = 0
     }
 
     init() {
         initialState = State()
-
     }
 
+}
+
+// MARK: - Mutate
+extension HomeReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewDidLoad:
+            return .just(.fetchQuestList)
+        }
+    }
+}
+
+// MARK: - Reduce
+extension HomeReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .fetchQuestList:
+            provider.rx.request(.fetchQuestList(type: "ALL"))
+            .subscribe { event in
+                switch event {
+                case let .success(response):
+                    if let data = try? JSONDecoder().decode(FetchQuestListResponseDTO.self, from: response.data) {
+                        newState.progress = data.progress
+                    }
+                case let .failure(error):
+                    print(error)
+                }
+            }.disposed(by: disposeBag)
+        }
+        return newState
+    }
 }
